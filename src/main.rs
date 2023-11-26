872ad64475f292c390a5e1a0eba188639a1abed5
TOwInOK <60252419+TOwInOK@users.noreply.github.com>
2023-11-27 03:40:19 +0700

    изменил адресс на деплой

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ async fn main() -> std::io::Result<()> {
                     .service(create),
             )
     })
-    .bind(("localhost", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
